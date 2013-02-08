8be4676de258c1cc068aefda3e507058055fa838
Jonathan Hamel <jonathan.hamel@ventyx.abb.com>
2013-02-08 14:36:36 -0500

    refs 23524
    Added additional unit test coverage

--- ventyx-sample-service/src/test/java/com/ventyx/sample/application/service/LocationServiceTest.java
+++ ventyx-sample-service/src/test/java/com/ventyx/sample/application/service/LocationServiceTest.java
@@ -44,7 +44,6 @@ public class LocationServiceTest  {
     @Test
     public void testGetLanguages() throws Exception {
 
-        /*  TODO Put back these tests
         try {
             locationService.getLanguages(null);
             fail("Invalid test case pass");
@@ -58,7 +57,6 @@ public class LocationServiceTest  {
         } catch (LocationServiceException ex) {
             assertTrue(ex.getMessage().equals("A valid country code is required."));
         }
-        */
 
         assertTrue(locationService.getLanguages("ZZZZZZZZZ") == null);
 
@@ -88,7 +86,6 @@ public class LocationServiceTest  {
     @Test
     public void testGetCountry() throws Exception {
 
-        /** TODO put back
         try {
             locationService.getCountry(null);
             fail("Invalid test case pass");
@@ -102,7 +99,6 @@ public class LocationServiceTest  {
         } catch (LocationServiceException ex) {
             assertTrue(ex.getMessage().equals("A valid country code is required."));
         }
-        **/
 
         assertTrue(locationService.getCountry("ZZZZZZZZZ") == null);
 
